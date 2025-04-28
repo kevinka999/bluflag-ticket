8efa1a1291b876a2595925c54dc825b903560ba0
kevinka999 <kevinka999@gmail.com>
2025-04-28 09:20:13 -0300

    feat: creating the pie graphic component for usage

--- src/components/PieGraphic.tsx
+++ src/components/PieGraphic.tsx
@@ -1,46 +1,20 @@
-import { Cell, ResponsiveContainer } from "recharts";
+import {
+  Cell,
+  ResponsiveContainer,
+  Pie,
+  PieChart,
+  Tooltip,
+  Legend,
+} from "recharts";
 
-import { Pie } from "recharts";
-
-import { PieChart } from "recharts";
-
-const data = [
-  { name: "Group A", value: 400 },
-  { name: "Group B", value: 300 },
-  { name: "Group C", value: 300 },
-  { name: "Group D", value: 200 },
-];
-
-const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
-
-const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = ({
-  cx,
-  cy,
-  midAngle,
-  innerRadius,
-  outerRadius,
-  percent,
-  index,
-}: any) => {
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-  const x = cx + radius * Math.cos(-midAngle * RADIAN);
-  const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
-  return (
-    <text
-      x={x}
-      y={y}
-      fill="white"
-      textAnchor={x > cx ? "start" : "end"}
-      dominantBaseline="central"
-    >
-      {`${(percent * 100).toFixed(0)}%`}
-    </text>
-  );
+type PieGraphicProps = {
+  data: { name: string; value: number }[];
+  tooltipFormatter?: (value: number) => string;
 };
 
-export const PieGraphic = () => {
+const COLORS = ["#1f65a2", "#E5E7EB"];
+
+export const PieGraphic = ({ data, tooltipFormatter }: PieGraphicProps) => {
   return (
     <ResponsiveContainer width="100%" height="100%">
       <PieChart>
@@ -48,16 +22,17 @@ export const PieGraphic = () => {
           data={data}
           cx="50%"
           cy="50%"
-          labelLine={false}
-          label={renderCustomizedLabel}
           outerRadius={80}
-          fill="#8884d8"
+          paddingAngle={0}
           dataKey="value"
         >
-          {data.map((entry, index) => (
+          {data.map((_entry, index) => (
             <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
           ))}
         </Pie>
+
+        <Tooltip formatter={tooltipFormatter} />
+        <Legend layout="horizontal" verticalAlign="bottom" align="center" />
       </PieChart>
     </ResponsiveContainer>
   );
